eac846eb8e7db87b93cdc56c48925dbf488b81bb
psrtech <contactus.psr@gmail.com>
2024-11-14 19:45:48 +0530

    started developing the Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/header';
+import logo from './logo.svg';
+import './App.css';
+import Login from './login';
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Login />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/login.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './css/login.css'
+
+function Login() {
+  
+
+  return (
+    <div className='maindiv' >
+        <div className="formdiv"></div>
+        <div className="infodiv"></div>
+    </div>
+  );
+}
+
+export default Login;
